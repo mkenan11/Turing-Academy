68e55a1e92e77c57cc8aa2f77197eee2da395c70
mkenan11 <mammedovkenan1117@gmail.com>
2025-01-15 18:22:20 +0400

    Add files via upload

---
+++ Grade Task JS Beginner/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let grade = prompt("Zəhmət olmasa qiyməti daxil edin:"); 
+
+if(grade > 90)
+    alert("Əlaçı")
+else if(grade >70 && grade<=90)
+    alert("Yaxşı")
+else if(grade >50 && grade<=70)
+    alert("Kafi")
+else
+    alert("Kəsildin!!!!!")

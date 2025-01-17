ba34ed26cb89b2181ba2bc3dfdcde1b7cf6581a4
mkenan11 <mammedovkenan1117@gmail.com>
2025-01-17 19:09:19 +0400

    Add files via upload

---
+++ Js Tasks Algorithms/script.js
@@ -0,0 +1,112 @@
+"use strict"
+
+//Task 1
+
+// let num1 = Number(prompt("Birinci Ədədi Daxil edin"))
+// let num2 = Number(prompt("Ikinci Ədədi Daxil edin"))
+// let num3 = Number(prompt("Ucuncu Ədədi Daxil edin"))
+
+// let maxNumber;
+
+// if(num1 > num2 && num1>num3){
+//     maxNumber = num1
+// }
+
+// else if(num2 > num1 && num2 > num3){
+//     maxNumber = num2
+// }
+
+// else{
+//     maxNumber = num3
+// }
+
+// alert("Ən Boyuk ədəd: " + maxNumber)
+
+
+//Task 2
+
+// let year = Number(prompt("İli daxil edin:"));
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     alert(year + " artıq ildir.");
+// } else {
+//     alert(year + " artıq il deyil.");
+// }
+
+
+//Task 3
+
+// let grade = Number(prompt("Qiymət Daxil Edin: "))
+
+
+// if(grade > 90){
+//     alert("Sənin Qiymətin A - dır")
+// }
+// else if(grade > 80){
+//     alert("Sənin Qiymətin B - dir")
+// }
+// else if(grade > 70){
+//     alert("Sənin Qiymətin C - dir")
+// }
+// else if(grade > 60){
+//     alert("Sənin Qiymətin D - dir")
+// }
+// else if(grade > 50){
+//     alert("Sənin Qiymətin E - dir")
+// }
+// else if(grade < 50){
+//     alert("Sənin Qiymətin F - dir")
+// }
+// else{
+//     alert("Sən Kəsildin!!")
+// }
+
+
+//Task 4
+
+// let number = Number(prompt("Ədəd Daxil Edin :"))
+
+// if( number%2 === 0){
+//     alert("Bu cüt ədəddir")
+// }
+// else if( number%2 === 1){
+//     alert("Bu cüt ədəd deyil əksinə tək ədəddir")
+// }
+// else{
+//     alert("Bu vapse eded deyil")
+// }
+
+
+//Task 5
+
+    // for (let i = 1; i <= 50; i++) {
+
+    //     if (i % 5 === 0 && i % 3 === 0) {
+    //         console.log("FizzBuzz")
+    //     }
+    
+    //     else if (i % 3 === 0) {
+    //         console.log("Fizz")
+    //     }
+    
+    //     else if (i % 5 === 0) {
+    //         console.log("Buzz")
+    //     }
+    //     else{
+    //         console.log(i)
+    //     }
+    // }
+
+
+    //Task 6
+
+    let number = Number(prompt("Ədəd Daxil Edin :"))
+
+    for( let j = 1; j <= 10 ; j++ ){
+        console.log( j * number )
+    }
+
+
+
+
+
+
